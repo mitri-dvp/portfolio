32800df8b8f2d2b61cba183a39bb7e3667811399
Jorge Mitri <mitri.dvp@gmail.com>
2021-03-12 12:29:12 -0300

    Desing change at Services and Games

--- components/Games.js
+++ components/Games.js
@@ -13,23 +13,17 @@ export default function Games() {
         <p className={styles.sub_title}>Want to have fun?</p>
 
         <div className={`${styles.tools_wrapper}`}>
-          <div>
-            <a rel="noopener noreferrer" className={styles.game} href='https://mitri-dvp.github.io/JavaScript-Whack-A-Mole/' target='_blank'>
-              <img src="/imgs/mole.png" alt="mole"/>
-            </a>
-            <a rel="noopener noreferrer" className={styles.link} href='https://mitri-dvp.github.io/JavaScript-Whack-A-Mole/' target='_blank'>Whack-A-Mole</a>
+          <div className={styles.game}>
+            <a rel="noopener noreferrer" href='https://mitri-dvp.github.io/JavaScript-Whack-A-Mole/' target='_blank'><img src="/imgs/mole.png" alt="mole"/></a>
+            <a className={styles.link} rel="noopener noreferrer" href='https://mitri-dvp.github.io/JavaScript-Whack-A-Mole/' target='_blank'><h3>Whack-A-Mole</h3></a>
           </div>
-          <div>
-            <a rel="noopener noreferrer" className={styles.game} href='https://mitri-dvp.github.io/JavaScript-Brain-Train/' target='_blank'>
-              <img src="/imgs/brain.png" alt="brain"/>
-            </a>
-            <a rel="noopener noreferrer" className={styles.link} href='https://mitri-dvp.github.io/JavaScript-Brain-Train/' target='_blank'> Brain Train</a>
+          <div className={styles.game}>
+            <a rel="noopener noreferrer" href='https://mitri-dvp.github.io/JavaScript-Brain-Train/' target='_blank'><img className={styles.img_fix} src="/imgs/brain.png" alt="brain"/></a>
+            <a className={styles.link} rel="noopener noreferrer" href='https://mitri-dvp.github.io/JavaScript-Brain-Train/' target='_blank'><h3>Brain Train</h3></a>
           </div>
-          <div>
-            <a rel="noopener noreferrer" className={styles.game} href='https://mitri-dvp.github.io/JavaScript-Snake/' target='_blank'>
-              <img src="/imgs/snake.png" alt="snake"/>
-            </a>
-            <a rel="noopener noreferrer" className={styles.link} href='https://mitri-dvp.github.io/JavaScript-Snake/' target='_blank'>Snake</a>
+          <div className={styles.game}>
+            <a rel="noopener noreferrer" href='https://mitri-dvp.github.io/JavaScript-Snake/' target='_blank'><img src="/imgs/snake.png" alt="snake"/></a>
+            <a className={styles.link} rel="noopener noreferrer" href='https://mitri-dvp.github.io/JavaScript-Snake/' target='_blank'><h3>Snake</h3></a>
           </div>
         </div>
 
