7776cc751eff494a76f14725af287b4d48a082ae
Jorge Mitri <mitri.dvp@gmail.com>
2021-04-01 22:59:21 -0300

    SEO optimization V0.1

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import Content from '../components/Content'
 import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
 
-
 export default function Home() {
   useEffect(() => {
     window.scrollTo(0, window.scrollY + 2)
@@ -16,11 +15,28 @@ export default function Home() {
     <div className={styles.wrapper}>
       <Head>
         <title>mitri.dvp | Full-Stack Developer</title>
-        <meta property="og:title" content="mitri.dvp" />
-        <meta name="google-site-verification" content="gPQZKSnJWApmajiOJUPLAXEwH8fLcntHdP8_kRENIpA" />
         <meta name="description" content="Hello, i’m Jorge Mitri, a Full-Stack Developer. I love building solutions for my clients, find out how I make things happen!"/>
-        <meta property="og:description" content="Hello, i’m Jorge Mitri, a Full-Stack Developer. I love building solutions for my clients, find out how I make things happen!"/>
+        <meta name="image" content="/imgs/thumbnail.png" />
+        <meta name="viewport" content="width=device-width,minimum-scale=1" /> 
         <link rel="icon" href="icon.svg" />
+
+        <link rel="canonical" href="https://www.mitri-dev.xyz/" /> 
+
+        <meta property="og:title" content="mitri.dvp" />
+        <meta property="og:description" content="Hello, i’m Jorge Mitri, a Full-Stack Developer. I love building solutions for my clients, find out how I make things happen!"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.mitri-dev.xyz/" /> 
+        <meta property="og:image" content="/imgs/thumbnail.png" /> 
+        <meta property="og:image:alt" content="mitri-dvp-thumbnail" /> 
+        <meta property="og:title" content="mitri.dvp" /> 
+
+        <meta name="twitter:card" content="summary" /> 
+        <meta name="twitter:site" content="@mitri_dvp" />
+        <meta property="twitter:title" content="mitri.dvp | Full-Stack Developer" /> 
+        <meta name="twitter:description" content="Hello, i’m Jorge Mitri, a Full-Stack Developer. I love building solutions for my clients, find out how I make things happen!" />
+        <meta name="twitter:image" content="/imgs/thumbnail.png" />
+
+        <meta name="google-site-verification" content="gPQZKSnJWApmajiOJUPLAXEwH8fLcntHdP8_kRENIpA" />
       </Head>
       
       <Header/>
